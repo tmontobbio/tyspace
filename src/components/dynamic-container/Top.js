7e179eaafcc8ec8cb5b8fea2c5feb9baf7eea964
Emma Cohanim <emmacohanim@gmail.com>
2022-08-23 22:30:17 -0700

    built top 8 shell

--- src/components/dynamic-container/Top.js
+++ src/components/dynamic-container/Top.js
@@ -2,5 +2,42 @@ import React from "react"
 import "./Top.css"
 
 export default function Top() {
-  return <div id="top">Top 8 TEST</div>
+  return (
+  <div id="top">
+    <h1>EMO KID's Friend Space</h1>
+    <div className="top" id="top1">
+      <h2>MySpace Tom</h2>
+      <img className="top-image" src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/3/31/1427823466140/1fe69f2c-59d6-4e07-ab3a-8b60dbe35db2-1020x1020.jpeg?width=700&quality=85&auto=format&fit=max&s=488d904c14758c38d8010de62c742e4b" alt="top1"/>
+    </div>
+    <div className="top" id="top2">
+      <h2>Friend 2</h2>
+      <img className="top-image" src="https://i.pinimg.com/originals/e5/dc/be/e5dcbeef346a333eead3baff22cffb1e.jpg" alt="top2"/>
+    </div>
+    <div className="top" id="top3">
+      <h2>Friend 3</h2>
+      <img className="top-image" src="https://i.pinimg.com/originals/ff/74/42/ff74420c8f4f9e4ef3bbad5681858277.jpg" alt="top3"/>
+    </div>
+    <div className="top" id="top4">
+      <h2>Friend 4</h2>
+      <img className="top-image" src="https://i.pinimg.com/736x/09/ea/d2/09ead24dff813a728f40182eade28511--emo-boys-youtubers.jpg" alt="top4" /> 
+    </div>
+    <div className="top" id="top5">
+      <h2>Friend 5</h2>
+      <img className="top-image" src="https://live.staticflickr.com/3228/2942282928_f72cea1b4a.jpg" alt="top5" />
+    </div>
+    <div className="top" id="top6">
+      <h2>Friend 6</h2>
+      <img className="top-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYZvBMahNyP4cH-VEbB2X2atBkFozBh51ILQ&usqp=CAU" alt="top6" />
+    </div>
+    <div className="top" id="top7">
+      <h2>Friend 7</h2>
+      <img className="top-image" src="https://a2-images.myspacecdn.com/images01/15/5fda24094cf1895930f8dfb05ac9c62f/300x300.jpg" alt="top7" />
+    </div>
+    <div className="top" id="top8">
+      <h2>Friend 8</h2>
+      <img className="top-image" src="https://a1-images.myspacecdn.com/images03/31/651291a9c3c6436cafa1a3118b089753/600x600.jpg" alt="top8" />
+    </div>
+
+    </div>
+  )
 }
